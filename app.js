9b2e853599f057815f2b2909699eafaf0d26d585
Annalise Murphy <nali247@gmail.com>
2020-03-31 18:55:24 -0700

    Lab02GuessingANumber

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-import compareNumbers from './compareNumbers.js'
+import compareNumbers from './compareNumbers.js';
 
 const submitButton = document.getElementById('myButton');
-const result = document.getElementById('guess-result');
-const triesDisplay = document.getElementById('tries-display');
+
+const triesSpan = document.getElementById('tries-span');
 const tooHighLow = document.getElementById('too-high-or-too-low');
 const winOrlose = document.getElementById('you-win-some-you-lose-some');
+const correctNumber = Math.ceil(Math.random() * 12);
 
-let correctNumber = Math.ceil(Math.random() * 12);
 let triesDisplay = 3;
 
 
+
 submitButton.addEventListener('click', () => {
-    compareNumbers(guess, correctNumber) 
-    triesDisplay--;
-    
-    
-}
-);
+    const userGuess = document.getElementById('user-guess').value;
+    const userNumber = Number(userGuess);
+    console.log(typeof userNumber);
+
+    const result = compareNumbers(userGuess, correctNumber) 
+    triesDisplay--; 
+    if (result === -1) {
+        triesSpan.textContent=('Too low!');
+        triesDisplay--;
+    } else if (result === 1) {
+        triesSpan.textContent=('Too high!');
+        triesDisplay--;
+    } else {
+        triesSpan.textContent=('You won!');
+    }
+
+
+});
